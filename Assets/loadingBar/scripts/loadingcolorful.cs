73a19c9efca92069f958ed37ad7b837b580c3470
Jojo17082023 <155248229+Jojo17082023@users.noreply.github.com>
2024-01-17 00:48:52 +0100

    Add files via upload

---
+++ Assets/loadingBar/scripts/loadingcolorful.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loadingcolorful : MonoBehaviour {
+
+    private RectTransform rectComponent;
+    private Image imageComp;
+    public float speed = 0.0f;
+
+    [Range(0, 1f)] [SerializeField] public float fillAmount = 0.0f;
+
+    // Use this for initialization
+    void Start () {
+        rectComponent = GetComponent<RectTransform>();
+        imageComp = rectComponent.GetComponent<Image>();
+        imageComp.fillAmount = 0.0f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        imageComp.fillAmount = fillAmount;
+
+    }
+}
