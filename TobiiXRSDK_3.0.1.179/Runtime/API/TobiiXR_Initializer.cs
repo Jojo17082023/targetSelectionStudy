1d3fbbe1f64aa5632f7311fce72dede61345257e
Jojo17082023 <155248229+Jojo17082023@users.noreply.github.com>
2024-01-17 01:57:46 +0100

    Add files via upload

---
+++ TobiiXRSDK_3.0.1.179/Runtime/API/TobiiXR_Initializer.cs
@@ -0,0 +1,23 @@
+﻿// Copyright © 2018 – Property of Tobii AB (publ) - All Rights Reserved
+
+using Tobii.XR;
+using UnityEngine;
+
+/// <summary>
+/// Optional convenience initializer for TobiiXR. Used by the TobiiXR Initializer prefab.
+///
+/// Feel free to replace this script with a manual call to TobiiXR.Start.
+/// 
+/// Ideally TobiiXR.Start should be called after any required VR SDK has been
+/// initialized but before any game object that use TobiiXR is called.
+/// </summary>
+[DefaultExecutionOrder(-10)] // Ausführungsreihenfolge - Priorität -10 d.h. Die Klasse wird von den meistern anderen Skritpen in der Unity Szene ausgeführt wird
+public class TobiiXR_Initializer : MonoBehaviour
+{
+    public TobiiXR_Settings Settings;
+    
+    private void Awake()
+    {
+        TobiiXR.Start(Settings);
+    }
+}
